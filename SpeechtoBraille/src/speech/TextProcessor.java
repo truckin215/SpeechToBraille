5611ba7f6d8e990c116cfe183e3d108d26a53a59
Connor Murray <connormurray215@gmail.com>
2025-03-04 12:25:06 -0500

    added assemblyAI

--- SpeechtoBraille/src/speech/TextProcessor.java
+++ SpeechtoBraille/src/speech/TextProcessor.java
@@ -1,5 +1,7 @@
 package speech;
 
+import java.io.File;
+
 // Make sure to add com.assemblyai:assemblyai-java to your dependencies
 
 import com.assemblyai.api.AssemblyAI;
@@ -7,21 +9,6 @@ import com.assemblyai.api.resources.transcripts.requests.TranscriptParams;
 import com.assemblyai.api.resources.transcripts.types.*;
 public final class TextProcessor {
 
-   // public TextProcessor() throws Exception {
-    	/*
-        AssemblyAI client = AssemblyAI.builder()
-                .apiKey("4531fb0bfce74443ab637fe7a8e73c28")
-                .build();
-
-        Transcript transcript = client.transcripts().transcribe("https://assembly.ai/news.mp4");
-
-        if (transcript.getStatus() == TranscriptStatus.ERROR) {
-            throw new Exception("Transcript failed with error: " + transcript.getError().get());
-        }
-
-        System.out.println("Transcript: " + transcript);
-    }
-    */
     
     public boolean record() {
     	SpeechListner recorder = new SpeechListner();
@@ -29,9 +16,24 @@ public final class TextProcessor {
     	return false;
     }
 
-	public void transcribe() {
-		// TODO Auto-generated method stub
-		
+	public void transcribe() throws Exception {
+		//Initializes AssemblyAi
+        AssemblyAI client = AssemblyAI.builder()
+                .apiKey("4531fb0bfce74443ab637fe7a8e73c28")
+                .build();
+        //get audio file
+        File audioFile = new File("output/recorded_audio.wav");
+        if (!audioFile.exists()) {
+            System.err.println("Error: Audio file not found.");
+            return;
+        }
+        Transcript transcript = client.transcripts().transcribe("https://assembly.ai/news.mp4");
+
+        if (transcript.getStatus() == TranscriptStatus.ERROR) {
+            throw new Exception("Transcript failed with error: " + transcript.getError().get());
+        }
+
+        System.out.println("Transcript: " + transcript);
 	}
 }
 
