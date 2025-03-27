1a623f3137b658e5320c1355b217cad517a8f52a
Connor Murray <connormurray215@gmail.com>
2025-03-27 13:46:38 -0400

    Revert "Done"
    This reverts commit e51dab3dbd18c0c6465bad128b9a9dc05c92a111.

--- SpeechtoBraille/src/speech/TextProcessor.java
+++ SpeechtoBraille/src/speech/TextProcessor.java
@@ -2,7 +2,6 @@ package speech;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -60,16 +59,6 @@ public final class TextProcessor {
 	    //runs through each character
 	    for (int i=0; i<script.length;i++){
 	    	brailleList.add(new BrailleChar(script[i]));
-	    }
-	    printList(brailleList);
-	}
-	public void printList(List<BrailleChar> brailleList) {
-	    for (int i=0; i<brailleList.size();i++){
-	    	int[][] value = brailleList.get(i).getValue();
-	    	for(int y=0; y<value.length;y++) {
-	    			System.out.println(Arrays.toString(value[y]));
-	    	}
-	    	System.out.println(" ");
 	    }
 	}
 }
